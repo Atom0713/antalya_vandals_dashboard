7f5c505430fcf76554dffbd1916a88791c6c7091
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2022-09-07 23:00:53 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-      <App />
+    <App />
   </BrowserRouter>
 );
