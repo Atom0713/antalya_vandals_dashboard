28815ad0638189b141471aa33891723aaca55ff9
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2023-01-05 14:00:42 +0100

    routes constant

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import  {Login, Home, User, Event, Events, Profile, ProtectedRoute, DepthChart, PageNotFound } from "./components/";
+import { ROUTES } from "./routes";
 
 export default function App() {
 
   return (
-    <Routes>
-      <Route path="/login" element={<Login />}></Route>
-      <Route element={<ProtectedRoute />}>
-        <Route path="/" 
-            element={
-                  <Home/>
-            }></Route>
-        <Route path="/me/:id" element={<Profile/>}></Route>
-        <Route path="/users" element={<User />}></Route>
-        <Route path="/events" element={<Events/>}></Route>
-        <Route
-            path="/event/:id"
-            element={<Event />}
-        ></Route>
-        <Route path="/depth_chart" element={<DepthChart/>}></Route>
-        <Route path="*" element={<PageNotFound/>}></Route>
-      </Route>
-    </Routes>
+    ROUTES
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import React from "react";
-
 import { Routes, Route } from "react-router-dom";
-import  {Login, Home, User, Event, Events, Profile } from "./components/";
+import  {Login, Home, User, Event, Events, Profile, ProtectedRoute, DepthChart, PageNotFound } from "./components/";
+
 
 export const ROUTES =
     <Routes>
-        <Route path="/" element={<Home/>}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route element={<ProtectedRoute />}>
+        <Route path="/" 
+            element={
+                    <Home/>
+            }></Route>
         <Route path="/me/:id" element={<Profile/>}></Route>
         <Route path="/users" element={<User />}></Route>
         <Route path="/events" element={<Events/>}></Route>
@@ -14,4 +18,7 @@ export const ROUTES =
             path="/event/:id"
             element={<Event />}
         ></Route>
+        <Route path="/depth_chart" element={<DepthChart/>}></Route>
+        <Route path="*" element={<PageNotFound/>}></Route>
+        </Route>
     </Routes>
